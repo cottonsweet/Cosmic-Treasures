6e4187041b38e8ce3e67abeb5bdaf7b3eba9a705
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-03-31 07:33:08 +0000

    시간 버튼 결정 동적구현

---
+++ src/pages/Card/SelectedTime.tsx
@@ -0,0 +1,52 @@
+// CSS
+import React, { useState }  from "react"
+import classes from  "./SelectedTime.module.sass"
+
+const timeList = [
+    {
+        id:1,
+        title:"일주일 뒤",
+        time:"week"
+    },
+    {
+        id:2,
+        title:"한 달 뒤",
+        time:"month"
+    },
+    {
+        id:3,
+        title:"일 년 뒤",
+        time:"year"
+    },
+    {
+        id:4,
+        title:"1주일 ~ 1년 랜덤",
+        time:"random"
+    },
+]
+
+const SelectedTime = () => {
+
+    const [choiceTime, setChoiceTime] = useState("");
+
+
+
+    const handleClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+        const value = (e.target as HTMLButtonElement).value
+        if (choiceTime === value) return setChoiceTime("")
+        setChoiceTime(value)
+    }
+    return(
+        <div className={classes.SelectedTime_item}>
+            {timeList.map((timeData) => {
+                const isSelected = timeData.time === choiceTime;
+                const className = `${classes[timeData.time]} ${isSelected ? classes.select : ""}`;
+                return(
+                    <button onClick={handleClick} key={timeData.id} value={timeData.time} className={className}>{timeData.title}</button>
+                )
+            })}
+        </div>
+    )
+}
+
+export default SelectedTime
