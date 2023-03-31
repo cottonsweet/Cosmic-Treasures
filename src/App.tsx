fad0490eb79135bfa9e3e1e32099de758461dea9
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-03-31 07:34:28 +0000

    불필요한 코드 및 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import Router from "./pages/Router";
-import "./Test.module.sass";
+
 
 const App = () => {
   return (
     <div className="App">
-      <p>
-        안녕하세요
-        <span>사스 테스트</span>
-      </p>
       <Router />
     </div>
   );
