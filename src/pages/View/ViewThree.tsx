ebb9e86800bba80c300dff777eb0bb7fafaabdb8
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-03 08:19:53 +0000

    뷰단 3번 컴포넌트 디자인 및 props 데이터 동적 출력

--- src/pages/View/ViewThree.tsx
+++ src/pages/View/ViewThree.tsx
@@ -1,12 +1,41 @@
+
+
+// Type
+import { DummyType } from "../Card/PlanetData"
+
+// CSS
+import classes from "./ViewThree.module.sass"
+
+
+// Components
+import SelectedTime from "../Card/SelectedTime"
+
 interface Props {
     onNext: () => void
     onPrev: () => void
+    selectedPlanet:DummyType
+    planetTitle: string,
+    plnaetDesc: string,
 }
 
 
 const ViewThree = (props:Props) => {
     return(
-        <div></div>
+        <div className={classes["view-three"]}>
+            <div className={classes["view-three__header"]}>
+                <div className={classes["view-three__img"]}>
+                    <span className={classes["view-three__img-item"]}>{props.selectedPlanet.img}</span>
+                </div>
+                
+                <div className={classes["view-three-planet__desc"]}>{props.selectedPlanet.desc}</div>
+                <div className={classes["view-three-planet__title"]}>{props.selectedPlanet.title}에서 이 타임캡슐을 언제 받아볼까요 ?</div>
+            </div>
+
+
+            <div className={classes["view-three__footer"]}>
+                <SelectedTime/>
+            </div>
+        </div>
     )
 }
 
