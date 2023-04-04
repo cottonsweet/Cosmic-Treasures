5d3b60aeb1921af448a4ad2a22f8612b7485144f
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-04 08:32:47 +0000

    재사용 button 컴포넌트 props 타입 정의

--- src/components/layouts/Button/Button.tsx
+++ src/components/layouts/Button/Button.tsx
@@ -3,18 +3,28 @@ import React from "react";
 import classes from "./Button.module.sass";
 
 interface Props {
-  title: string;
+  title?:string,
   className?: string;
   onClick?: (e: React.MouseEvent) => void;
+  selectedTimList?:TimeProps
+  value?:string,
+}
+
+interface TimeProps {
+  id:number,
+  title:string,
+  time:string,
 }
 
 // 각 버튼들 재사용할수있게끔
 const Button = (props: Props) => {
   const buttonClass = props.className ? classes[props.className] : "";
   return (
-    <button className={buttonClass} onClick={props.onClick}>
-      {props.title}
+    <button className={buttonClass} key={props.selectedTimList?.id} onClick={props.onClick} value={props.selectedTimList?.time}>
+      {props.title ? props.title : props.selectedTimList?.title}
     </button>
+
+
   );
 };
 
