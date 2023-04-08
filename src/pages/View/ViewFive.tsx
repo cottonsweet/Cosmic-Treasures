369926d5c7241e3a8811b84955bf59e13347effd
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-08 18:05:16 +0000

    ViewFive page 컴포넌트 뷰단 작업

--- src/pages/View/ViewFive.tsx
+++ src/pages/View/ViewFive.tsx
@@ -1,12 +1,37 @@
+import { useNavigate } from "react-router-dom"
+
+// Type
+import { DummyType } from "../../Type/DataType"
+
+// CSS
+import classes from "./ViewFive.module.sass"
+
+// Components
+import Button from "../../components/layouts/Button/Button"
+import RocketIcon from "../../components/layouts/Icons/RocketIocn"
+
 interface Props {
-    onNext: () => void
-    onPrev: () => void
+    selectedPlanet: DummyType
 }
 
 
 const ViewFive = (props:Props) => {
+
+    const path = useNavigate()
+
+    const handleGoHome = () => path("/")
+
+
     return(
-        <div></div>
+        <div className={classes["view-five"]}>
+            <span className={classes["rocket-icon"]}><RocketIcon/></span>
+            <div className={classes["view-five__success"]}>
+                <div>타임 캡슐을</div>
+                <div>{props.selectedPlanet.desc}행성에</div>
+                <div>안전하게 보냈어요</div>
+            </div>
+            <Button title="홈으로" onClick={handleGoHome} className="view-five__nextHome" />
+        </div>
     )
 }
 
