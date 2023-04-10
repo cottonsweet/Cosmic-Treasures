528447603721fec91330709e15742b114a250a5d
LK1 <64725837+sstipdev@users.noreply.github.com>
2023-04-10 08:20:28 +0000

    AuthWrap props children 타입 지정

--- src/components/Common/Wrap/Auth/AuthWrap.tsx
+++ src/components/Common/Wrap/Auth/AuthWrap.tsx
@@ -1,9 +1,16 @@
+import React from "react"
+
+
+interface Props {
+  children:React.ReactNode
+}
+
 // CSS
 import classes from "./AuthWrap.module.sass";
 
 // 회원가입, 로그인 페이지를 감싸는 wrapper
-function AuthWrap({ children }: any) {
-  return <div className={classes["AuthWrap"]}>{children}</div>;
+function AuthWrap(props:Props) {
+  return <div className={classes["AuthWrap"]}>{props.children}</div>;
 }
 
 export default AuthWrap;
